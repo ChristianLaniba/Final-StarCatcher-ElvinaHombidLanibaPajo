7b61b04e304c3994474f984008b8dbc7e9cd83dd
ChristianLaniba <cLaniba@mcm.edu.ph>
2026-06-27 23:45:05 +0800

    Initial commit

---
+++ scenes/Boot.js
@@ -0,0 +1,37 @@
+export default class Boot extends Phaser.Scene {
+    constructor() {
+        super({ key: 'Boot' });
+    }
+
+    preload() {
+        this.load.image("bg", "assets/bg.png");
+        this.load.image("mainGround", "assets/tiles/ground.png");
+        this.load.image("platform", "assets/tiles/platform.png");
+        this.load.spritesheet("player", "assets/player.png", {
+            frameWidth: 540,
+            frameHeight: 540
+        });
+        this.load.spritesheet("patrolEnemy", "assets/patrolEnemy.png", {
+            frameWidth: 540,
+            frameHeight: 540
+        });
+        this.load.spritesheet("chaseEnemy", "assets/chaseEnemy.png", {
+            frameWidth: 540,
+            frameHeight: 540
+        });
+        this.load.image("star", "assets/star.png");
+        this.load.image("bomb", "assets/bomb.png");
+        this.load.image("heart", "assets/heart.png");
+        this.load.image("particle", "assets/particle.png");
+
+        this.load.audio('jump', 'assets/sounds/jump.mp3');
+        this.load.audio('collect', 'assets/sounds/collect.mp3');
+        this.load.audio('hurt', 'assets/sounds/hurt.mp3');
+        this.load.audio('gameover', 'assets/sounds/gameover.mp3');
+        this.load.audio('bgm', 'assets/sounds/bgm.mp3');
+    }
+
+    create() {
+        this.scene.start('Game');
+    }
+}
